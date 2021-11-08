91c1d3d1c2be85f09d37b06813d57e1534ddce34
Theoneste SIKUBWABO <sikutheo11@gmail.com>
2021-11-08 03:27:12 -0800

    django authantication

--- Rweb/settings.py
+++ Rweb/settings.py
@@ -141,3 +141,19 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.INFO: 'danger',
+    50: 'critical',
+}
+
+# SMTP CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = '587'
+EMAIL_HOST_USER = 'rusks1142@gmail.com'
+EMAIL_HOST_PASSWORD = 'Rusgrdsb11@'
+EMAIL_USE_TLS = True
+
--- accounts/models.py
+++ accounts/models.py
@@ -3,22 +3,23 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 class MyAccountManager(BaseUserManager):
-	def create_user(self, first_name, last_name, username, email, password= None):
-		if not email:
-			raise ValueError('User must have an email')
+	def create_user(self, first_name, last_name, username, email, password=None):
+	    if not email:
+	        raise ValueError('User must have an email address')
 
-		if not username:
-			raise ValueError(' User must have an username')
-		user = self.model(
-			email = self.normalize_email(email) ,
-			username = username,
-			first_name = first_name,
-			last_name = last_name,
+	    if not username:
+	        raise ValueError('User must have an username')
 
-			)
-		user.set_password(password)
-		user.save(using = self._db)
-		return user
+	    user = self.model(
+	        email = self.normalize_email(email),
+	        username = username,
+	        first_name = first_name,
+	        last_name = last_name,
+	    )
+
+	    user.set_password(password)
+	    user.save(using=self._db)
+	    return user
 
 	def create_superuser(self, first_name, last_name, username, email, password):
 		user = self.create_user(
