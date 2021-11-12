5fa2c72ee329762625aebc6ae71c2d6b02031acb
Theoneste SIKUBWABO <sikutheo11@gmail.com>
2021-11-12 04:40:22 -0800

    cart and cart Items for user

--- accounts/views.py
+++ accounts/views.py
@@ -14,6 +14,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
+import requests
 
 # Create your views here.
 
@@ -109,7 +110,17 @@ def login(request):
 
             auth.login(request, user)
             messages.success(request, 'Logged successful')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            
+            try:
+                query = requests.utils.urlparse(url).query
+                # next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)                
+            except:
+                return redirect('dashboard')
 
         else:
         	messages.error(request, 'Invalid email or password')
