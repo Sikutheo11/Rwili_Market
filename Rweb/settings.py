439d50fc9af8b073e9b6be79c33509088083b559
Theoneste SIKUBWABO <sikutheo11@gmail.com>
2021-12-13 02:56:36 -0800

    new config

--- Rweb/settings.py
+++ Rweb/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default= True, cast = bool)
 
-ALLOWED_HOSTS = ['rwili-market-env.eba-5pk2c4un.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
