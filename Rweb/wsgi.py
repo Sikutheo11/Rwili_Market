35ba89c13b2b2bf3e83ddb3b187b8268e279eb06
Theoneste SIKUBWABO <sikutheo11@gmail.com>
2021-12-30 11:22:27 -0800

    Allowed host1

--- Rweb/wsgi.py
+++ Rweb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for greatkar project.
+WSGI config for Rweb project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
