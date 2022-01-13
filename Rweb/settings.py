2024e7a06ad57f15650565753bca3ffaf6964982
Theoneste SIKUBWABO <sikutheo11@gmail.com>
2022-01-13 05:52:50 -0800

    error 500

--- Rweb/settings.py
+++ Rweb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['rwili.herokuapp.com/','127.0.0.1']
 
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 #STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
