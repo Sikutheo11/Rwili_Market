39a2adbdecfed6ba4fd2bb6ccc6af8cc021e4615
Theoneste SIKUBWABO <sikutheo11@gmail.com>
2022-01-16 00:51:54 -0800

    static

--- Rweb/settings.py
+++ Rweb/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(str(BASE_DIR), 'db.sqlite3'),
     }
 }
 
